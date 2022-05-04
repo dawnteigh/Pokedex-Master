4f829da99510bc2c04f8e3fa6874b18ebe0579e0
Donte Ladatto <donte279@gmail.com>
2022-05-04 15:59:31 -0500

    create component hierarchy

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import '../App.css';
+import React from 'react'
+import Home from './Home'
+import Play from './Play'
+import Pokedex from './Pokedex'
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Play />
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;

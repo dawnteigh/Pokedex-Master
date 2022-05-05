9c4af16d4554fcd27038b771c7d68ee5c3af6493
Donte Ladatto <donte279@gmail.com>
2022-05-05 14:21:20 -0500

    Introductory paragraph and button rendering logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,16 @@ import DexButton from './DexButton'
 import { dexData } from '../DexData'
 
 const Home = () => {
+
     console.log(dexData)
+    const renderButtons = dexData.map(obj => <DexButton key={obj.region} dexData={obj} />)
   return (
-    <div>Home</div>
+    <div>
+        <p>
+            Welcome to Pokedex Master! Upon clicking the play tab above, you will be given a Pokedex entry for a random Pokemon. Your job is simple; guess the Pokemon! Each correct answer 'captures' the Pokemon and adds it to your own Pokedex. The game includes all Pokemon through Generation VIII by default, but if you're unfamiliar with some of the newer ones, don't fret! You can change the size of the Pokedex you want to play with using the buttons below. Happy collecting!
+        </p>
+        {renderButtons}
+    </div>
   )
 }
 

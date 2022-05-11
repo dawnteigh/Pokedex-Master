21be8480b17f74e2c81715c249b11c85cadb4f51
Donte Ladatto <donte279@gmail.com>
2022-05-11 12:33:27 -0500

    Add sorting state and radio css

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -5,6 +5,7 @@ import PokemonCard from './PokemonCard'
 const Pokedex = () => {
 
     const [pokedex, setPokedex] = useState([])
+    const [sort, setSort] = useState("order")
 
     useEffect(() => {
         fetch("http://localhost:6001/pokemon")
@@ -33,25 +34,64 @@ const Pokedex = () => {
     })
 
     const handleSort = (e) => {
-        if (e.target.id === "alpha") {
+        if (e.target.id === "order") {
+            fetch("http://localhost:6001/pokemon")
+            .then(r => r.json())
+            .then(data => setPokedex(data))
+        }
+        else if (e.target.id === "alpha") {
             setPokedex(alphaDex)
         }
         else if (e.target.id === "num") {
             setPokedex(numDex)
         }
+        setSort(e.target.id)
     }
 
+    const checkedStyle = {
+        background: "#ee07e2",
+        boxShadow: "rgba(238, 7, 226, 0.35) 0 -25px 18px -14px inset, rgba(238, 7, 226, 0.35) 0 1px 2px, rgba(238, 7, 226, 0.35) 0 2px 4px, rgba(238, 7, 226, 0.35) 0 4px 8px, rgba(238, 7, 226, 0.35) 0 8px 16px, rgba(238, 7, 226, 0.35) 0 16px 32px"
+        }
+
   return (
-    <div>
-        <div onChange={handleSort} >
-            Sort By:
-            <br/>
-            <input type="radio" id="alpha" name="sort" /> Alphabetical
-            <input type="radio" id="num" name="sort" /> Dex Number
-        </div>
+    <div className="pokedex">
+        Sort By:
+        <br/>
+        <form>
+            <label style={(sort === "order" ? checkedStyle : null)} >
+                <input
+                type="radio"
+                id="order"
+                name="sort"
+                checked={sort === "order"}
+                onChange={handleSort}
+                />
+                Order Caught
+            </label>
+            <label style={(sort === "alpha" ? checkedStyle : null)} >
+                <input
+                type="radio"
+                id="alpha"
+                name="sort"
+                checked={sort === "alpha"}
+                onChange={handleSort}
+                /> 
+                Alphabetical
+            </label>
+            <label style={(sort === "num" ? checkedStyle : null)} >
+                <input
+                type="radio"
+                id="num"
+                name="sort"
+                checked={sort === "num"}
+                onChange={handleSort}
+                />
+                Dex Number
+            </label>
+        </form>
         {(pokedex.length === 0) ? 
         <p>You haven't caught any Pokemon yet!</p> : 
-        <div className="pokedex">{displayPokedex}</div>}
+        <div className="pokedexGrid">{displayPokedex}</div>}
     </div>
   )
 }

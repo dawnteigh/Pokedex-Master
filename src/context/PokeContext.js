88bd35237f0cf6286a687dbee9c1c56d61f6a0de
Donte Ladatto <donte279@gmail.com>
2023-06-16 18:30:10 -0500

    rangeChange now invokes newPokemon to ensure a range-relevant entry

--- src/context/PokeContext.js
+++ src/context/PokeContext.js
@@ -27,49 +27,51 @@ function PokeProvider({ children }) {
     background: "#ee07e2",
     boxShadow: "rgba(238, 7, 226, 0.35) 0 -25px 18px -14px inset, rgba(238, 7, 226, 0.35) 0 1px 2px, rgba(238, 7, 226, 0.35) 0 2px 4px, rgba(238, 7, 226, 0.35) 0 4px 8px, rgba(238, 7, 226, 0.35) 0 8px 16px, rgba(238, 7, 226, 0.35) 0 16px 32px"
     }
-
-  const rangeChange = (obj) => {
-    setRange(obj)
-    history.push('/play')
-  }
-
+    
   const newPokemon = () => {
     let dexNo = Math.floor(Math.random() * (max - min) + min)
     if (!caught.includes(dexNo)) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${dexNo}`)
-      .then(r => r.json())
-      .then(data =>
-        setPokemon({
-          id: data.id,
-          name: data.species.name,
-          sprite: data.sprites.front_default,
-          types: data.types
-        })
-      )
-      .catch(() => alert('Failed to fetch Pokémon. Refresh to try again.'))
-    if (dexNo > 905) {
-      const pkmn = PaldeaEntries.find(p => p.number === dexNo)
-      const entry = pkmn.entries[Math.floor(Math.random() * pkmn.entries.length)]
-      setEntry(entry)
-    }
-    else {
-      fetch(`https://pokeapi.co/api/v2/pokemon-species/${dexNo}`)
-          .then(r => r.json())
-          .then(data => {
-            if (!data.flavor_text_entries[0]) {
-              setEntry('This Pokémon has no Pokédex entries yet! Use the hints below to guess.')
-            }
-            else {
-              const entries = data.flavor_text_entries.filter(e => e.language.name === 'en')
-              const randomEntry = entries[Math.floor(Math.random() * entries.length)]
-              setEntry(randomEntry.flavor_text)
-            }
+      fetch(`https://pokeapi.co/api/v2/pokemon/${dexNo}`)
+        .then(r => r.json())
+        .then(data =>
+          setPokemon({
+            id: data.id,
+            name: data.species.name,
+            sprite: data.sprites.front_default,
+            types: data.types
           })
-          .catch(() => alert('Failed to fetch Pokédex entry. Refresh to try again.'))
+        )
+        .catch(() => alert('Failed to fetch Pokémon. Refresh to try again.'))
+        if (dexNo > 905) {
+          const pkmn = PaldeaEntries.find(p => p.number === dexNo)
+          const entry = pkmn.entries[Math.floor(Math.random() * pkmn.entries.length)]
+          setEntry(entry)
         }
-      } else {
-        newPokemon()
+        else {
+          fetch(`https://pokeapi.co/api/v2/pokemon-species/${dexNo}`)
+              .then(r => r.json())
+              .then(data => {
+                if (!data.flavor_text_entries[0]) {
+                  setEntry('This Pokémon has no Pokédex entries yet! Use the hints below to guess.')
+                }
+                else {
+                  const entries = data.flavor_text_entries.filter(e => e.language.name === 'en')
+                  const randomEntry = entries[Math.floor(Math.random() * entries.length)]
+                  setEntry(randomEntry.flavor_text)
+                }
+              })
+              .catch(() => alert('Failed to fetch Pokédex entry. Refresh to try again.'))
+          }
       }
+    else {
+      newPokemon()
+      }
+  }
+  
+  const rangeChange = (obj) => {
+    setRange(obj)
+    newPokemon()
+    history.push('/play')
   }
 
   useEffect(() => {

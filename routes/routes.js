5df22b8f6c4f55285d2947f8539e30a1f58de34a
Donte Ladatto <donte279@gmail.com>
2023-08-04 19:05:53 -0500

    update get and post endpoints

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const Pokemon = require('../models/pokemon')
 
 const router = express.Router()
 // get a user's pokedex
-router.get('/getAll', async (req, res) => {
+router.get('/pokemon', async (req, res) => {
     try {
         const pokedex = await Pokemon.find()
         res.json(pokedex)
@@ -13,7 +13,7 @@ router.get('/getAll', async (req, res) => {
     }
 })
 // add a pokemon to the dex
-router.post('/post', async (req, res) => {
+router.post('/pokemon', async (req, res) => {
     const pokemon = new Pokemon({
         number: req.body.number,
         name: req.body.name,
@@ -47,13 +47,31 @@ router.get('/getOne/:id', async (req, res) => {
 })
 
 //Update by ID Method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedPokemon = req.body;
+        const options = { new: true };
+
+        const result = await Pokemon.findByIdAndUpdate(
+            id, updatedPokemon, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 //Delete by ID Method
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const pokemon = await Model.findByIdAndDelete(req.params.id)
+        res.send(`${pokemon.name} has been released! Bye bye, ${pokemon.name}!`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
-
 module.exports = router;

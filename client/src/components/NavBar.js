8ee494d8d02d3dd046b41aa22b0918b40c4e79c9
Donte Ladatto <donte279@gmail.com>
2023-10-14 15:34:16 -0500

    add logout and switch save buttons

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PokeContext } from "../context/PokeContext";
 import { NavLink } from "react-router-dom";
 import PokedexMaster from '../PokedexMaster.png'
 
+const NavBar = () => {
 
-function NavBar() {
+  const { setSaveFile, logout } = useContext(PokeContext)
 
-  const active = { 
+  const active = {
     background: "#ee07e2",
     boxShadow: "rgba(238, 7, 226, 0.35) 0 -25px 18px -14px inset, rgba(238, 7, 226, 0.35) 0 1px 2px, rgba(238, 7, 226, 0.35) 0 2px 4px, rgba(238, 7, 226, 0.35) 0 4px 8px, rgba(238, 7, 226, 0.35) 0 8px 16px, rgba(238, 7, 226, 0.35) 0 16px 32px"
   }
-  
+
   return (
     <div className="navBar">
       <div className="header">
@@ -39,6 +41,11 @@ function NavBar() {
       >
         Pokédex
       </NavLink>
+      <div className="nav-btns">
+        <button onClick={() => setSaveFile(false)}>Switch Save</button>
+        |
+        <button onClick={() => logout()}>Logout</button>
+      </div>
     </div>
   );
 }

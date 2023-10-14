496ebd8bd6df7413db81bbecb2053af46d007083
Donte Ladatto <donte279@gmail.com>
2023-10-14 15:32:45 -0500

    update answer fetch

--- client/src/components/AnswerForm.js
+++ client/src/components/AnswerForm.js
@@ -5,7 +5,7 @@ import Toast from 'react-bootstrap/Toast'
 
 const AnswerForm = ({ handleCaught, pokeFormat }) => {
 
-  const { pokemon, mode, newPokemon } = useContext(PokeContext)
+  const { pokemon, mode, newPokemon, saveFile, setErrors } = useContext(PokeContext)
   const { number, name, sprite, types } = pokemon
 
   const [answer, setAnswer] = useState("")
@@ -14,9 +14,9 @@ const AnswerForm = ({ handleCaught, pokeFormat }) => {
     message: "",
     img: ""
   })
-  
+
   const configObj = {
-    method: "POST",
+    method: "PATCH",
     headers: {
       "Content-Type": "application/json"
     },
@@ -28,30 +28,31 @@ const AnswerForm = ({ handleCaught, pokeFormat }) => {
     }),
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     if (pokeFormat(answer).toLowerCase() === name.toLowerCase()) {
-      fetch("http://localhost:4000/api/pokemon", configObj)
-      .then(r => r.json())
-      .then(data => {
-        console.log(`Successfully added ${data.name} to your Pokédex!`)
+      const r = await fetch(`/api/pokedex/${saveFile}/pokemon`, configObj)
+      const data = await r.json()
+      if (data.error) {
+        setErrors(data.error)
+      }
+      else {
         setNewPkmn({
           message: `Successfully added ${data.name.charAt(0).toUpperCase() + data.name.slice(1)} to your Pokédex!`,
           img: data.sprite
         })
         setShow(true)
         handleCaught(data.number)
-      })
-      .catch(() => alert("Something went wrong. Is your JSON Server running?"))
-    } else {
-        console.log('Oh no, the Pokémon got away!')
-        setNewPkmn({
-          message: 'Oh no, the Pokémon got away!',
-          img: mode === "easy" ? sprite : ""
-        })
-        setShow(true)
-        newPokemon()
       }
+    }
+    else {
+      setNewPkmn({
+        message: 'Oh no, the Pokémon got away!',
+        img: mode === "easy" ? sprite : ""
+      })
+      setShow(true)
+      newPokemon()
+    }
     e.target.reset()
   }
 
@@ -65,13 +66,13 @@ const AnswerForm = ({ handleCaught, pokeFormat }) => {
           </Toast.Header>
           <Toast.Body>
             <img src={newPkmn.img} alt="" />
-            <br/>
+            <br />
             {newPkmn.message}
           </Toast.Body>
         </Toast>
       </ToastContainer>
       <form onSubmit={handleSubmit} >
-        <input type="text" size="25" placeholder="Enter Pokémon name here!" onChange={(e) => setAnswer(e.target.value)}/>
+        <input type="text" size="25" placeholder="Enter Pokémon name here!" onChange={(e) => setAnswer(e.target.value)} />
         <input type="submit" value="Go!" />
       </form>
     </div>

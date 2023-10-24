930c4114addf89f0040eb429fda5c9ac135e7e23
Donte Ladatto <donte279@gmail.com>
2023-10-24 14:44:05 -0500

    loader portal and css changes

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { createPortal } from 'react-dom'
 import masterBall from '../assets/masterball.png'
 
 const Loader = () => {
   return (
-    <div id="loader">
-      <img src={masterBall} alt="Master Ball loader" className="loading" />
-    </div>
+    <>
+      {createPortal(
+        <div id="loader">
+          <img src={masterBall} alt="Master Ball loader" className="loading" />
+        </div>, document.body)}
+    </>
+
   )
 }
 

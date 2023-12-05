0db53ee025d97caffcff302fc7c385fa5fc1230e
Donte Ladatto <donte279@gmail.com>
2023-12-05 15:35:56 -0600

    add dropdown component

---
+++ client/src/components/Dropdown.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { PokeContext } from '../context/PokeContext'
+
+const Dropdown = ({ answer, setAnswer }) => {
+  const { pokemonList } = useContext(PokeContext)
+  const filterNames = pokemonList.filter(name => name.toLowerCase().match(answer.toLowerCase()))
+  const displayNames = filterNames.map(n => <li onClick={() => setAnswer(n)}>{n}</li>)
+
+  return (
+    <div>
+      <ul>
+        {answer.length > 0 && filterNames[0].toLowerCase() != answer.toLowerCase() && displayNames}
+      </ul>
+    </div>
+  )
+}
+
+export default Dropdown

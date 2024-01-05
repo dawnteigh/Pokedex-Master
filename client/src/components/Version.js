4671b551c8a1028b668b91f387ee47754ea0dc45
Donte Ladatto <donte279@gmail.com>
2024-01-04 21:50:11 -0600

    v2.0.2

--- client/src/components/Version.js
+++ client/src/components/Version.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Version = () => {
   return (
     <div id="version">
-      <h3 className='screen-header'>Version 2.0.1</h3>
+      <h3 className='screen-header'>Version 2.0.2</h3>
       <h6>Features</h6>
       <ul>
         <li>Account creation</li>
         <li>Save files</li>
         <li>Improved UI</li>
-        <li>All Pokémon as of the Teal Mask DLC</li>
+        <li>All Pokémon as of the Indigo Disk DLC</li>
         <li>Assistive answer dropdown</li>
       </ul>
       <br />
@@ -23,7 +23,6 @@ const Version = () => {
         <li>Loading spinner</li>
         <li>Improved Pokédex filters</li>
         <li>Milestones and Medals</li>
-        <li>New Pokémon from the Indigo Disk DLC</li>
       </ul>
     </div>
   )
